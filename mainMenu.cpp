37c9685f8c73e85a7a8dff9fce4087482aab2850
Zach Nett <zjnett@mac.com>
2019-09-28 22:38:21 -0700

    Added new game text

--- mainMenu.cpp
+++ mainMenu.cpp
@@ -12,15 +12,23 @@ void mainMenu::runMainMenu(sf::RenderWindow &window) {
     sf::Font sysfont;
     sysfont.loadFromFile("assets/fonts/Pelagiad.ttf");
     
-
     // Create logo object and set properties
     sf::Text logo("Causality", sysfont);
     logo.setCharacterSize(250);
     logo.setFillColor(sf::Color::White);
-    logo.setPosition(window.getSize().x / 2 - (logo.getLocalBounds().width / 2), window.getSize().y / 10);
+    logo.setPosition(window.getSize().x / 2 - (logo.getLocalBounds().width / 2), 
+        window.getSize().y / 10);
+
+    // Create new game UI button
+    sf::Text newGame("New Game", sysfont);
+    newGame.setCharacterSize(100);
+    newGame.setFillColor(sf::Color::White);
+    newGame.setPosition(window.getSize().x / 2 - (newGame.getLocalBounds().width / 2), 
+        logo.getPosition().y + logo.getLocalBounds().height + (2 * newGame.getLocalBounds().height));
 
     // Window clear, object display hierarchy, display command
     window.clear();
     window.draw(logo);
+    window.draw(newGame);
     window.display();
 }
